b9d53b678f329322cb59821ca71ec2a5f4e66284
damilog <dami.k.dev@gmail.com>
2021-03-05 02:50:42 +0900

    fix :충돌 해결

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,8 @@
 import MyPromise from "./MyPromise.js";
+<<<<<<< HEAD
 import { sendSignal } from "./receiveSignal.js";
 
 //메세지를 받는다?
 sendSignal();
+=======
+>>>>>>> 499c0e9 (feat : 송수신기 UI html, sass 추가)
