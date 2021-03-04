45127fa926bd9655ff705c777a573949c7ba2071
damilog <dami.k.dev@gmail.com>
2021-03-04 16:58:54 +0900

    feat : 받은 단어를 2초마다 해석해서 출력하기, 화살표 돌리기, 해석된 단어 보여주기 기능 추가

--- public/javascripts/receiveSignal.js
+++ public/javascripts/receiveSignal.js
@@ -38,28 +38,22 @@ const sendSignal = hexList => {
   const sender = setInterval(() => {
     if (idx === hexadecimals.length) {
       clearInterval(sender);
+      showInterpretedHexStr(interpretHexStr($inputReceive.value)); // 수신 완료 시 버튼 활성화 -> 버튼 클릭시 show 되게
       console.log("전송 완료");
     } else {
       console.log(hexadecimals[idx]);
       receiveHexSignal(hexadecimals[idx]); //["6","4"]형태로 전달
       idx++;
     }
-  }, 5000);
+  }, 3000);
 };
 
-const sendSignalToArrow = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve();
-    }, 1000);
-  });
-};
 const receiveHexSignal = data => {
   const hexadecimals = data; //["6","4"]형태로 전달받음
   console.log(hexadecimals, "받음");
 
   const transmitter = new Promise((resolve, reject) => {
-    setTimeout(() => resolve(hexadecimals), 2000);
+    setTimeout(() => resolve(hexadecimals), 1000);
   });
 
   transmitter
@@ -70,30 +64,47 @@ const receiveHexSignal = data => {
     })
     .then(hex => {
       return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(hex), 2000);
+        setTimeout(() => resolve(hex), 1000);
       });
     })
     .then(hex => {
       console.log(hex[1], "1번 rotate에 보냄");
       rotateArrow(hex[1]);
+    })
+    .finally(hex => {
+      rotateArrow(hex, true);
     });
 };
 
-const rotateArrow = hex => {
+const rotateArrow = (hex, finished = false) => {
   //화살표를 회전시키는 함수 hex값에 해당하는 deg만큼 화살표를 회전시킴
 
   $arrow.style.transform = `rotate(${hexLocations[hex]}deg)`;
-  showReceivedHexStr(hex); //input에 현재 hex값을 보여줌
-  colorReceivedHexStr(hex); //현재 hex값에 해당하는 elem을 깜빡거리게 함
+  showReceivedHexStr(hex, finished); //input에 현재 hex값을 보여줌
+  //colorReceivedHexStr(hex); //현재 hex값에 해당하는 elem을 깜빡거리게 함
 };
 
-const showReceivedHexStr = str => {
-  //input에 현재 hex값을 보여줌
-  let totalStr;
-  totalStr = +str;
-  $inputReceive.value = totalStr;
+const saveReceivedHexStr = str => {};
+
+const showReceivedHexStr = (str, finished) => {
+  finished ? ($inputReceive.value += " ") : ($inputReceive.value += str);
+
+  console.log($inputReceive.value);
 };
 
+const showInterpretedHexStr = str => {
+  const $inputInterpret = _.$("#input-interpret");
+  $inputInterpret.value = str;
+};
+
+const interpretHexStr = str => {
+  const hexStr = str
+    .split(" ")
+    .filter(x => x)
+    .map(x => parseInt(x, 16));
+  const char = String.fromCharCode(...hexStr);
+  return char;
+};
 const colorReceivedHexStr = key => {
   console.log(key);
   const $currentHex = _.$(`#text-${key}`);
