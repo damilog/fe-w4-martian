c59e5895d7d541d67824536463107110fe367ed3
damilog <dami.k.dev@gmail.com>
2021-03-04 21:11:33 +0900

    ffix: 해석하기 버튼 이벤트가 미리 실행되는 오류 수정

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,4 @@
 import MyPromise from "./MyPromise.js";
 import { sendSignal, convertCharToHex } from "./receiveSignal.js";
 
-sendSignal(convertCharToHex("daisy"));
+sendSignal(convertCharToHex("crong"));
--- public/javascripts/receiveSignal.js
+++ public/javascripts/receiveSignal.js
@@ -1,9 +1,10 @@
 import _ from "./util.js";
 const $arrow = _.$(".arrow");
 const $inputReceive = _.$(".input-receive");
+const $btnInterpret = _.$(".btn-interpret");
 const hexLocations = {
   Total: 360,
-  0: 10,
+  0: 10, // -350 -(total - 0)
   1: 30,
   2: 60,
   3: 80,
@@ -13,16 +14,19 @@ const hexLocations = {
   7: 170,
   8: 190,
   9: 210,
-  A: 240,
-  B: 260,
-  C: 280,
-  D: 300,
-  E: 330,
-  F: 350,
+  a: 240,
+  b: 260,
+  c: 280,
+  d: 300,
+  e: 330,
+  f: 350,
 };
 
 //const go = (...args) => reduce((acc, fun) => fun(acc), args);
-
+const resetReceive = () => {
+  clearInterval(sender);
+  $arrow.style.transform = "";
+};
 const convertCharToHex = word => {
   const currentWord = [...word];
   return currentWord
@@ -38,7 +42,7 @@ const sendSignal = hexList => {
   const sender = setInterval(() => {
     if (idx === hexadecimals.length) {
       clearInterval(sender);
-      showInterpretedHexStr(interpretHexStr($inputReceive.value)); // 수신 완료 시 버튼 활성화 -> 버튼 클릭시 show 되게
+      $btnInterpret.disabled = false;
       console.log("전송 완료");
     } else {
       console.log(hexadecimals[idx]);
@@ -46,50 +50,6 @@ const sendSignal = hexList => {
       idx++;
     }
   }, 3000);
-};
-
-const receiveHexSignal = data => {
-  const hexadecimals = data; //["6","4"]형태로 전달받음
-  console.log(hexadecimals, "받음");
-
-  const transmitter = new Promise((resolve, reject) => {
-    setTimeout(() => resolve(hexadecimals), 1000);
-  });
-
-  transmitter
-    .then(hex => {
-      console.log(hex[0], "0번 rotate에 보냄");
-      rotateArrow(hex[0]);
-      return hex;
-    })
-    .then(hex => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(hex), 1000);
-      });
-    })
-    .then(hex => {
-      console.log(hex[1], "1번 rotate에 보냄");
-      rotateArrow(hex[1]);
-    })
-    .finally(hex => {
-      rotateArrow(hex, true);
-    });
-};
-
-const rotateArrow = (hex, finished = false) => {
-  //화살표를 회전시키는 함수 hex값에 해당하는 deg만큼 화살표를 회전시킴
-
-  $arrow.style.transform = `rotate(${hexLocations[hex]}deg)`;
-  showReceivedHexStr(hex, finished); //input에 현재 hex값을 보여줌
-  //colorReceivedHexStr(hex); //현재 hex값에 해당하는 elem을 깜빡거리게 함
-};
-
-const saveReceivedHexStr = str => {};
-
-const showReceivedHexStr = (str, finished) => {
-  finished ? ($inputReceive.value += " ") : ($inputReceive.value += str);
-
-  console.log($inputReceive.value);
 };
 
 const showInterpretedHexStr = str => {
@@ -105,10 +65,76 @@ const interpretHexStr = str => {
   const char = String.fromCharCode(...hexStr);
   return char;
 };
-const colorReceivedHexStr = key => {
-  console.log(key);
-  const $currentHex = _.$(`#text-${key}`);
-  $currentHex.classList.add("blink");
+
+const receiveHexSignal = data => {
+  const hexadecimals = data; //["6","4"]형태로 전달받음
+  console.log(hexadecimals, "받음");
+
+  const transmitter = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(hexadecimals), 1000);
+  });
+
+  transmitter
+    .then(hex => {
+      console.log(hex[0], "0번 rotate에 보냄");
+      activateReceiver(hex[0]);
+      return hex;
+    })
+    .then(hex => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(hex), 1000);
+      });
+    })
+    .then(hex => {
+      console.log(hex[1], "1번 rotate에 보냄");
+      activateReceiver(hex[1]);
+    })
+    .finally(hex => {
+      activateReceiver(hex, true);
+    });
 };
 
+const activateReceiver = (hex, finished = false) => {
+  rotateArrow(hex, finished); //화살표를 회전시키는 함수 hex값에 해당하는 deg만큼 화살표를 회전시킴
+  showReceivedHexStr(hex, finished); //input에 현재 hex값을 보여줌
+  colorReceivedHexStr(hex, finished);
+};
+
+const rotateArrow = (hex, finished) => {
+  if (!finished) $arrow.style.transform = `rotate(${hexLocations[hex]}deg)`;
+};
+
+const calculateDistance = hex => {
+  //가까운 쪽으로 움직이는거 ..
+  const currentArrowRotation = $arrow.style.transform
+    .replace("rotate(", "")
+    .replace("deg)", "");
+  //알아서 가까운 쪽으로 움직이는거 같은데..?
+};
+
+const showReceivedHexStr = (str, finished) => {
+  finished ? ($inputReceive.value += " ") : ($inputReceive.value += str);
+};
+
+const colorReceivedHexStr = (key, finished) => {
+  if (finished) return;
+
+  const $currentHex = _.$(`#text-${key}`);
+
+  if (!_.$(".blink")) {
+    $currentHex.classList.add("blink");
+  } else {
+    const $blinkingHex = _.$(".blink");
+    $blinkingHex.classList.remove("blink");
+    $currentHex.classList.add("blink");
+  }
+};
+
+const onEvent = () => {
+  $btnInterpret.addEventListener(
+    "click",
+    () => showInterpretedHexStr(interpretHexStr($inputReceive.value)) //정답 보여주기
+  );
+};
+onEvent();
 export { sendSignal, convertCharToHex };
